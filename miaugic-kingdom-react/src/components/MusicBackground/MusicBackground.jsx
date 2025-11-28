133d831cf2e28fbf69e743171056937e7a935cc2
Enya <enyaglf.2000@gmail.com>
2025-11-28 20:08:00 +0100

    finalizo el proyecto creo el componenete de musica de fondo, aniado icono al navegador, corrijo documentacion

---
+++ miaugic-kingdom-react/src/components/MusicBackground/MusicBackground.jsx
@@ -0,0 +1,41 @@
+//-----------COMPONENETE MUSIC BACKGROUND PARA MUSICA DE FONDO----------------//
+/**La funcionalidad de este componenete es reproducir una musica continua
+ * indiferentemente de si se renderiza el componenete, si se recarga la pagina la musica comienza de nuevo */
+import { useEffect, useRef } from "react";
+
+export default function MusicBackground() {
+  const audioRef = useRef(null);
+  const TARGET_VOLUME = 0.04; // Volumen de la musica de fondo
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    audio.volume = TARGET_VOLUME;
+    audio.muted = false; // simpre desmutear
+    audio.loop = true; // reproducir en bucle
+
+    // Algunos navegadores bloquean la reproduccion automatica de audio
+    // hasta que el usuario interactua con la pagina (click, tecla, etc)
+
+    const activarPorClick = () => {
+      audio.play();
+      window.removeEventListener("click", activarPorClick);
+    };
+
+    audio.play().catch(() => {});
+    window.addEventListener("click", activarPorClick);
+
+    return () => {
+      window.removeEventListener("click", activarPorClick);
+    };
+  }, []);
+
+  return (
+    <audio
+      ref={audioRef}
+      src="/sonidos/Golden Brown V2.mp3" // Ruta al archivo de audio
+      style={{ display: "none" }}
+    />
+  );
+}
